065bf7e372ffbad96dfeddbef19ced5e49ac3fbb
arurz <solod1467@gmail.com>
2021-08-10 09:33:28 +0300

    services added

---
+++ GameStore/GameStore/Services/RoleService.cs
@@ -0,0 +1,17 @@
+﻿using GameStore.Contexts;
+using GameStore.Models;
+
+namespace GameStore.Services
+{
+    public class RoleService
+    {
+        public void CreateRole(Role role)
+        {
+            using(var context = new GameStoreDBContext())
+            {
+                context.Roles.Add(role);
+                context.SaveChanges();
+            }
+        }
+    }
+}
