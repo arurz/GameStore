61a87630e6b2ad2c3f9f60292eb8a3d6e7d6549d
Artur Solod <solod1467@gmail.com>
2023-06-26 16:08:10 +0300

    database(creation): appLogContext created

---
+++ GameStoreApi/GameStoreApi.Data/Errors/Error.cs
@@ -0,0 +1,15 @@
+﻿using GameStoreApi.Data.Common.Interfaces;
+using System;
+
+namespace GameStoreApi.Data.Errors
+{
+	public class Error : IEntity
+	{
+		public int Id { get; set; }
+		public int StatusCode { get; set; }
+		public string Message { get; set; }
+		public string Verb { get; set; }
+		public string Url { get; set; }
+		public DateTime ErrorAppearanceDateTime { get; set; }
+	}
+}
--- GameStoreApi/GameStoreApi.Persistence/AppLogContext.cs
+++ GameStoreApi/GameStoreApi.Persistence/AppLogContext.cs
@@ -1,6 +1,28 @@
-﻿namespace GameStoreApi.Persistence
+﻿using GameStoreApi.Data.Errors;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System;
+
+namespace GameStoreApi.Persistence
 {
-	public class AppLogContext
+	public class AppLogContext : DbContext
 	{
+        public DbSet<Error> Errors{ get; set; }
+        public AppLogContext(DbContextOptions<AppLogContext> options) : base(options)
+		{ }
+
+		public override int SaveChanges()
+		{
+			var entries = ChangeTracker
+				.Entries()
+				.Where(x => x.State == EntityState.Added);
+
+			foreach (var entry in entries)
+			{
+				entry.Property("ErrorAppearanceDateTime").CurrentValue = DateTime.Now;
+			}
+
+			return base.SaveChanges();
+		}
 	}
 }
