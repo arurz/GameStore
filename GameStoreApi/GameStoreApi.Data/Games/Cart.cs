0c14f25ce1bd0043de3e3f711e80f7a8c2d606aa
Artur Solod <solod1467@gmail.com>
2023-06-26 16:06:21 +0300

    server(refactor): extra dependencies&classes removed

--- GameStoreApi/GameStoreApi.Data/Games/Cart.cs
+++ GameStoreApi/GameStoreApi.Data/Games/Cart.cs
@@ -3,11 +3,7 @@ using GameStoreApi.Data.Users;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameStoreApi.Data.Games
 {
