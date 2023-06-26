3acb7d314c85347a885e7b15c2767d2af03f9faf
Artur Solod <solod1467@gmail.com>
2023-06-26 16:10:51 +0300

    server(feature): created service for DbConnection injections

--- GameStoreApi/GameStoreApi.Hosting/Startup.cs
+++ GameStoreApi/GameStoreApi.Hosting/Startup.cs
@@ -1,31 +1,24 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GameStoreApi.Infrastructure.DependencyInjections;
 
 namespace GameStoreApi.Hosting
 {
 	public class Startup
 	{
+		public IConfiguration Configuration { get; }
 		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
 		}
 
-		public IConfiguration Configuration { get; }
-
-		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-
+			services.AddDBConnections(Configuration);
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
 			{
@@ -33,7 +26,6 @@ namespace GameStoreApi.Hosting
 			});
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
---
+++ GameStoreApi/GameStoreApi.Infrastructure/DependencyInjections/ConfigureServicesDbConnection.cs
@@ -0,0 +1,21 @@
+﻿using GameStoreApi.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GameStoreApi.Infrastructure.DependencyInjections
+{
+	public static class ConfigureServicesDbConnection
+	{
+		public static IServiceCollection AddDBConnections(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddDbContext<AppDbContext>(
+				options => options.UseNpgsql(configuration["DBConnection:AppConnectionString"]));
+
+			services.AddDbContext<AppLogContext>(
+				options => options.UseNpgsql(configuration["DBConnection:LogConnectionString"]));
+
+			return services;
+		}
+	}
+}
