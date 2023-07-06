e5ab10701022259b3b0095f7537d85a52b0ef475
Artur Solod <solod1467@gmail.com>
2023-07-06 20:08:27 +0300

    server(fix): added missing dtos

---
+++ GameStoreApi/GameStoreApi.Data/Users/Dtos/NewPasswordDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStoreApi.Data.Users.Dtos
+{
+	public class NewPasswordDto
+	{
+        public int Id { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+		public string RepeatNewPassword { get; set; }
+	}
+}
---
+++ GameStoreApi/GameStoreApi.Data/Users/Dtos/UserSettingsDto.cs
@@ -0,0 +1,12 @@
+﻿namespace GameStoreApi.Data.Users.Dtos
+{
+	public class UserSettingsDto
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Username { get; set; }
+		public string Email { get; set; }
+		public string TelephoneNumber { get; set; }
+	}
+}
