a4119ccdb88a630b7540b074980b99fa4b853dba
EURE\QX238WJ <artur.solod@bg.ey.com>
2023-09-04 17:42:51 +0300

    server(feature): connection string for azure sql server database stored in project secrets

--- GameStoreApi/GameStoreApi.Infrastructure/DependencyInjections/ConfigureServicesDbConnection.cs
+++ GameStoreApi/GameStoreApi.Infrastructure/DependencyInjections/ConfigureServicesDbConnection.cs
@@ -9,13 +9,16 @@ namespace GameStoreApi.Infrastructure.DependencyInjections
 	{
 		public static IServiceCollection AddDBConnections(this IServiceCollection services, IConfiguration configuration)
 		{
-			services.AddDbContext<AppDbContext>(
-				options => options.UseNpgsql(configuration["DBConnection:AppConnectionString"]));
+			//services.AddDbContext<AppDbContext>(
+			//	options => options.UseNpgsql(configuration["DBConnection:AppConnectionString"]));
 
-			services.AddDbContext<AppLogContext>(
-				options => options.UseNpgsql(configuration["DBConnection:LogConnectionString"]));
+			//services.AddDbContext<AppLogContext>(
+			//	options => options.UseNpgsql(configuration["DBConnection:LogConnectionString"]));
 
-			return services;
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration["ConnectionString:GameStoreAppDb:SqlDb"]));
+            services.AddDbContext<AppLogContext>(options => options.UseSqlServer(configuration["DBConnection:LogConnectionString"]));
+
+            return services;
 		}
 	}
 }
